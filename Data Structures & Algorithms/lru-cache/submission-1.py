c354b71c0dae803bea75412dcb18e731b0f208ad
Kaushik Dudhade <80742370+Kaushik-Dudhade@users.noreply.github.com>
2026-06-23 16:28:08 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self,key,val):
+        self.key, self.val = key,val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def get(self, key: int) -> int:
+        if not key in self.cache:
+            return -1
+        
+        self.remove(self.cache[key])
+        self.insert(self.cache[key])
+
+        return self.cache[key].val
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+        
+
+        
+    def insert(self, node):
+        prev,nxt = self.right.prev, self.right
+        prev.next =node
+        node.prev = prev
+        node.next = nxt
+        nxt.prev = node
+    
+    def remove(self, node):
+        prv,nxt = node.prev, node.next
+        prv.next,nxt.prev = nxt, prv
+
+
+
+
+
